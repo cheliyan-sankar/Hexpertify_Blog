9d8381287e2e2d0b628fb21a5ef3b08a9b51802d
Hexpertify <hexpertifyapp@gmail.com>
2026-02-21 21:05:41 +0530

    Add base path and disable experimental features
    Set base path for the application and comment out experimental server actions.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath:"/blogs",
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['localhost:3000', 'localhost:3001', 'scaling-spork-jj9jvg9rvrjq2pv4p-3001.app.github.dev'],
-    },
-  },
+  // experimental: {
+  //   serverActions: {
+  //     allowedOrigins: ['localhost:3000', 'localhost:3001', 'scaling-spork-jj9jvg9rvrjq2pv4p-3001.app.github.dev'],
+  //   },
+  // },
 };
 
 module.exports = nextConfig;
